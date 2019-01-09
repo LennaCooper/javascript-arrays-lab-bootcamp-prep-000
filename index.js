112bc4650227c8bb00523952313a0db66ad46f18
Lenna Cooper <lenna.cooper@gmail.com>
2019-01-09 18:20:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] 
 
-// Add your functions and code here
+function destructivelyAppendKitten(name) {
+ kittens.push("Ralph")
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift("Bob")
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+ appendKitten = [...kittens, "Broom"]
+ return (appendKitten)
+}
+
+function prependKitten(name) {
+  prependKitten = ["Arnold", ...kittens]
+  return (prependKitten)
+}
+
+function removeLastKitten() {
+  kittens.slice(0, kittens.length -1)
+  return (removeLastKitten)
+}
