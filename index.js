955cb48e0f90b826e822fe1edeaf5f7b8cc318ac
Lenna Cooper <lenna.cooper@gmail.com>
2019-01-09 18:22:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  kittens.slice(0, kittens.length - 1)
-  return (kittens)
+  removeLastKitten = kittens.slice(0, kittens.length - 1)
+  return (removeLastKitten)
 }
